5b43a0de00fdef971e4a29814267f7b901572acc
Jean-Pierre <jpcuvelliez@gmail.com>
2020-05-14 00:04:57 +0200

    .

--- src/view/HomeView.tsx
+++ src/view/HomeView.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { RouteComponentProps } from 'react-router'
 import Template from '../Template'
+import logo from '../logo.svg'
 
 const HomeView: React.FC<RouteComponentProps> = props => {
   return (
     <Template>
       <div>Home</div>
+      <img src={logo}/>
     </Template>
   )
 }
